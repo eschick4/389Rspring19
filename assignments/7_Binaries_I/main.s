ff843ec26ced957e8574f89e6ab7e3fcb41ddb50
eschick4 <erin.schick@hotmail.com>
2019-04-26 13:28:28 -0400

    commiting new assignment

--- assignments/7_Binaries_I/main.s
+++ assignments/7_Binaries_I/main.s
@@ -2,28 +2,28 @@ section .text
 
 global main
 main:
-  push rbp
-  mov rbp, rsp
-  sub rsp, 0x10
-  mov DWORD [rbp-0x8], 0x1ceb00da
-  mov DWORD [rbp-0x4], 0xfeedface
+  push rbp # pushing rbp to the stack
+  mov rbp, rsp # rbp to rsp
+  sub rsp, 0x10 # rsp=-0x10
+  mov DWORD[rbp-0x8], 0x1ceb00da # 
+  mov DWORD[rbp-0x4], 0xfeedface # 
 
-  mov eax, DWORD [rbp-0x4]
+  mov eax, DWORD[rbp-0x4]
   mov esi, eax
   mov edi, a_fmt  ; see data section at bottom
   mov eax, 0x0
-  call printf
+  call printf 
 
-  mov eax, DWORD [rbp-0x8]
+  mov eax, DWORD[rbp-0x8]
   mov esi, eax
   mov edi, b_fmt  ; see data section at bottom
   mov eax, 0x0
   call printf
 
-  mov eax, DWORD [rbp-0x8]
-  xor DWORD [rbp-0x4],eax
-  mov eax, DWORD [rbp-0x4]
-  xor DWORD [rbp-0x8],eax
+  mov eax, DWORD[rbp-0x8]
+  xor DWORD[rbp-0x4],eax
+  mov eax, DWORD[rbp-0x4]
+  xor DWORD[rbp-0x8],eax
   mov eax, DWORD [rbp-0x8]
   xor DWORD [rbp-0x4],eax
 
